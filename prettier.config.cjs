8f824984453ab8ff09813b75f2a58876e72a8d58
robinlow <30585316+robinlow@users.noreply.github.com>
2024-01-25 18:26:39 +0800

    Initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'none',
+	plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-sort-json', 'prettier-plugin-tailwindcss'],
+	importOrder: ['^node:', '', '<BUILT_IN_MODULES>', '<THIRD_PARTY_MODULES>', '', '^(@\\/)', '', '^[~]', '', '^[.]'],
+	importOrderTypeScriptVersion: '5.3.2'
+}
