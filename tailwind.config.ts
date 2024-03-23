aa6819b0d7cdf153fd9dd9741ae0961ea78397d0
robinlow <132737499+robinyk@users.noreply.github.com>
2024-03-23 20:24:42 +0800

    Update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
 export default {
 	content: ['./app/**/*.{js,jsx,ts,tsx}'],
 	darkMode: 'class',
@@ -6,4 +7,4 @@ export default {
 		extend: {}
 	},
 	plugins: []
-}
+} satisfies Config
